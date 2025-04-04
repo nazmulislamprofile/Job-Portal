0ed5003d80027631b40a9de92af09baff8c43183
nazmulislamprofile <nazmulislam01973@gmail.com>
2025-04-04 18:54:05 +0600

    error solve client

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import JobListing from '../components/JobListing';
-import AppDownload from '../components/AppDownLoad';
+import AppDownload from '../components/AppDownload';
 import Footer from '../components/Footer';
 
 const Home = () => {
